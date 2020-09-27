62bedde0db60cfbed6c15b4933568479757b73c7
kyu08 <kyu08@users.noreply.github.com>
2020-09-27 13:49:00 +0900

    add @P02 case of list.length == 1

--- src/main/scala/P02.scala
+++ src/main/scala/P02.scala
@@ -6,7 +6,7 @@ object P02 {
 
     def recursive(list: List[Int]): Int = list match {
       // length が 1 だったら例外を投げる
-//       case h :: Nil => throw new Error("length is 1")
+       case h :: Nil => throw new Error("length is 1")
       // length が 2 になるまで先頭を削除
       case a :: b :: c ::  _ => recursive(list.tail)
       // length が 2 になったら先頭を返す
