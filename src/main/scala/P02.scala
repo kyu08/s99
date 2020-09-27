8deeab6a56376ab424d2dec1a64edaa6a72c509e
kyu08 <kyu08@users.noreply.github.com>
2020-09-27 13:43:41 +0900

    modify P02

--- src/main/scala/P02.scala
+++ src/main/scala/P02.scala
@@ -2,13 +2,15 @@ package s99
 object P02 {
   def answer: Int = {
     val list = List(1,2,3,4,5)
+//    list.init.last
+
     def recursive(list: List[Int]): Int = list match {
       // length が 1 だったら例外を投げる
 //       case h :: Nil => throw new Error("length is 1")
       // length が 2 になるまで先頭を削除
-      case a :: b :: _ => recursive(list.tail)
+      case a :: b :: c ::  _ => recursive(list.tail)
       // length が 2 になったら先頭を返す
-      case c :: d => c
+      case a:: b :: c => a
       case _ => throw new Error("input is Nil")
     }
 
