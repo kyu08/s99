839b741f305fb1d6724926667a028af8dd6bba08
kyu08 <kyu08@users.noreply.github.com>
2020-10-01 13:07:14 +0900

    add P06 comment

--- src/main/scala/P06.scala
+++ src/main/scala/P06.scala
@@ -6,7 +6,10 @@ object P06 {
     // これだと 偶数個のときが考慮できてない。
     def recursive(list: List[Int]): Boolean = list match {
       case Nil => throw new NoSuchElementException
+      // 要素数が1つなら true
       case h :: Nil => true
+      // 先頭と末尾が等しければ再帰
+      // 等しくなければ false
       // case (h :: other) :+ h => { って書きたいけどそれはむりそう
       case (h :: other) :+ t =>
         if(h == t) return recursive(other)
