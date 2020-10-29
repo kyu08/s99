c7d0a6ae5fcd611b6af729b7955d9f04ff1ea437
kyu08 <kyu08@users.noreply.github.com>
2020-10-29 13:14:23 +0900

    P23 とちゅう(Randomが使えるようになったところから)

---
+++ src/main/scala/P23.scala
@@ -0,0 +1,18 @@
+package s99
+
+import _root_.scala.util.Random
+
+object P23 {
+  def answer(): Unit = {
+    val num = 3
+    val list = List('a, 'b, 'c, 'd, 'f, 'g, 'h)
+//    val r = new Random
+//    def hoge(i: Int, ls: List[Symbol]): List[Symbol] = (i, ls) match {
+//      case (_, Nil) => Nil
+//      case (1, _) => hoge()
+//      case (i, _) => ls(Random.netxInt(ls.length)) :: hoge(i - 1, ls)
+//    }
+    println(Random.nextInt())
+
+  }
+}
